6f2130f6e217221089c343378bc7f09985d397fb
Ben Ranford <84072202+ben-ranford@users.noreply.github.com>
2026-02-15 13:19:02 +1100

    lopper-rolling rolling-20260215020340-320a1f7

---
+++ Formula/lopper-rolling.rb
@@ -0,0 +1,18 @@
+class LopperRolling < Formula
+  desc "Local-first CLI/TUI for measuring dependency surface area"
+  homepage "https://github.com/ben-ranford/lopper"
+  url "https://github.com/ben-ranford/lopper/archive/refs/tags/rolling-20260215020340-320a1f7.tar.gz"
+  sha256 "e454bfa8f5b67b6a95bb800d6474edebec36e36b381134b4efe839d7ef48efba"
+  license "MIT"
+
+  depends_on "go" => :build
+  keg_only :versioned_formula
+
+  def install
+    system "go", "build", *std_go_args(output: bin/"lopper"), "./cmd/lopper"
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/lopper --help")
+  end
+end
